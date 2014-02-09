98b94215b09ffdad28bf8b0605c6d7917214b5d8
Jared Cosulich <jared@irrationaldesign.com>
2014-02-08 17:18:30 -0800

    rope with hinged segments

--- test2/Assets/Scripts/PlayerController.cs
+++ test2/Assets/Scripts/PlayerController.cs
@@ -3,8 +3,7 @@ using UnityEngine;
 
 [RequireComponent(typeof(PlayerPhysics))]
 
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
 
 	// Player Handling
 	public float gravity = 20;
@@ -47,40 +46,54 @@ public class PlayerController : MonoBehaviour
 		playerPhysics.Move (amountToMove * Time.deltaTime);
 
     if (Input.GetMouseButtonDown (0)) {
-      BoxCollider boxCollider = GetComponent<BoxCollider> ();
-
-      Vector3 topOfPlayerPosition = transform.position;
-
-      topOfPlayerPosition.x -= boxCollider.size.x/2;
-      topOfPlayerPosition.y += boxCollider.size.y/2;
-
-      Vector3 screenPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-      screenPoint.z = 0;
-
-      Vector3 ropePosition = new Vector3(
-        topOfPlayerPosition.x - ((topOfPlayerPosition.x - screenPoint.x) / 2),
-        topOfPlayerPosition.y - ((topOfPlayerPosition.y - screenPoint.y) / 2)
-      );
-
-      Vector3 relativePos = screenPoint - ropePosition;
-      Quaternion ropeRotation = Quaternion.LookRotation(relativePos);
-
-      float ropeLength = Mathf.Sqrt(
-        Mathf.Pow(topOfPlayerPosition.x - screenPoint.x, 2f) + 
-        Mathf.Pow(topOfPlayerPosition.y - screenPoint.y, 2f)
-      );
-      Vector3 ropeScale = new Vector3(0.05f, 0.05f, ropeLength);
-
-      rope.hingeJoint.connectedBody = transform.rigidbody;
-      rope.hingeJoint.anchor = new Vector3(0f, 0f, -ropeLength/2f);
-
-      Transform ropeTransform = (Instantiate (rope, ropePosition, ropeRotation) as GameObject).transform;
-      ropeTransform.localScale = ropeScale;
-
+      ThrowRope (Camera.main.ScreenToWorldPoint(Input.mousePosition));
     }
 	}
 
-	private float IncrementTowards (float current, float target, float acceleration) {
+  private void ThrowRope (Vector3 screenPoint) {
+    screenPoint.z = 0;
+    
+    BoxCollider boxCollider = GetComponent<BoxCollider> ();
+    
+    Vector3 topOfPlayerPosition = transform.position;
+    
+    topOfPlayerPosition.x -= boxCollider.size.x/2;
+    topOfPlayerPosition.y += boxCollider.size.y/2;
+    
+    float ropeLength = Mathf.Sqrt(
+      Mathf.Pow(topOfPlayerPosition.x - screenPoint.x, 2f) + 
+      Mathf.Pow(topOfPlayerPosition.y - screenPoint.y, 2f)
+      );
+    
+    float segmentLength = 0.25f;
+    Vector3 segmentScale = new Vector3(0.05f, 0.05f, segmentLength);
+    rope.hingeJoint.anchor = new Vector3(0f, 0f, -segmentLength/2f);
+    
+    Rigidbody connection = transform.rigidbody;
+    float segmentsCount = ropeLength/segmentLength;
+    
+    float xDistance = (topOfPlayerPosition.x - screenPoint.x) / segmentsCount;
+    float yDistance = (topOfPlayerPosition.y - screenPoint.y) / segmentsCount;
+
+    for (float i=0f; i<segmentsCount; ++i) {
+      rope.hingeJoint.connectedBody = connection;
+      
+      Vector3 ropePosition = new Vector3(
+        topOfPlayerPosition.x - (xDistance * (i + 0.5f)),
+        topOfPlayerPosition.y - (yDistance * (i + 0.5f))
+      );
+      
+      Vector3 relativePos = screenPoint - ropePosition;
+      Quaternion ropeRotation = Quaternion.LookRotation(relativePos);
+      
+      Transform ropeTransform = (Instantiate (rope, ropePosition, ropeRotation) as GameObject).transform;
+      ropeTransform.localScale = segmentScale;
+      connection = ropeTransform.rigidbody;
+    }
+  }
+
+
+  private float IncrementTowards (float current, float target, float acceleration) {
 		if (current == target) {
 			return current;
 		} else {
