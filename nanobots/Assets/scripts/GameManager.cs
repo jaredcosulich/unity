87b632becd6a476f76aea35aa5e3bdf60a339b98
Jared Cosulich <jared@irrationaldesign.com>
2014-02-23 12:37:14 -0800

    spawning nanobots

---
+++ nanobots/Assets/scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject bot;
+	public ArrayList bots;
+
+	// Use this for initialization
+	void Start () {
+		SpawnBot ();
+		SpawnBot ();
+		SpawnBot ();
+		SpawnBot ();
+		SpawnBot ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	private void SpawnBot() {
+		bots.Add ((Instantiate (bot, Vector3.zero, Quaternion.identity) as GameObject).transform);
+	}
+}
