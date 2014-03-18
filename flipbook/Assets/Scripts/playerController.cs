754545faab4dad7b082292949e5f02d67eb3b2f9
Jared Cosulich <jared@irrationaldesign.com>
2014-03-18 13:50:13 -0700

    flipbook code

---
+++ flipbook/Assets/Scripts/playerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerController : MonoBehaviour
+{
+	
+	private Animator animator;
+	
+	// Use this for initialization
+	void Start()
+	{
+		animator = this.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		
+		var horizontal = Input.GetAxis("Horizontal");
+		
+		if (horizontal > 0) {
+			animator.SetInteger ("Direction", 1);
+		} else if (horizontal < 0) {
+			animator.SetInteger ("Direction", -1);
+		} else {
+			animator.SetInteger ("Direction", 0);
+		}
+	}
+}
